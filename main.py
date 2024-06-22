53f4798c1b8573f7b3d57be277436695557f1d4e
David Pereira dos Anjos <168911003+DavidAnjos12@users.noreply.github.com>
2024-06-22 11:43:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print(f"seja muito bem vindo!")
+N1=float(input("Quanto você ganha por hora?: "))
+N2=float(input("Quantas horas você trabalha no mês?: "))
+
+N3 = (N1*N2)
+
+print(f" seu salário por mês é de {N3} reais")
